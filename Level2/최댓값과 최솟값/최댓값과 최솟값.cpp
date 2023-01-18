6eef3e65d9dc3670ff2f7a868676a6e8d724955f
JaeBro97 <fe99fe99@naver.com>
2023-01-18 17:04:41 +0900

    cording test Level2 - 1
    최댓값과 최솟값 구하기

---
+++ Level2/최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,38 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    string sTemp = "";
+    vector<int> iTemp;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            sTemp.push_back(s[i]);
+        }
+        else
+        {
+            iTemp.push_back(stoi(sTemp));
+            sTemp.clear();
+        }
+    }
+
+    iTemp.push_back(stoi(sTemp));
+    sort(iTemp.begin(), iTemp.end());
+    answer += to_string(iTemp[0]) + " " + to_string(iTemp[iTemp.size() - 1]);
+
+    cout << answer << endl;
+
+    return answer;
+}
+
+int main()
+{
+    solution({2,6,8,14});
+}
